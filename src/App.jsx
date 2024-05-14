690df26090627b9aef3327fda3eb57dcd6637560
Craig <craigthompson85@gmail.com>
2024-05-14 13:44:42 -0600

    Step 1 - added input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <label htmlFor="picture">Picture URL: </label>
+      <input type="text" id="picture" />
+      <label htmlFor="name">Name: </label>
+      <input type="text" id="name" />
+
+      <button type="button">Add Friend</button>
+    </div>
+  );
 }
